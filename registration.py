43d5d8f35274adc57cc4a5b12b5440d2f6730e27
akshatamulagundamath26 <akshatamulagundamath26@gmail.com>
2025-12-02 12:26:33 +0530

    Update registration.py

--- registration.py
+++ registration.py
@@ -1,15 +1,17 @@
-import pytest
-def registration_datails(Student_ID,Student_Name,Course_Enroled,Academic_Year):
-    result= (
-        f"Student_ID:{id}\n"
-        f"Student_Name:{name}\n"
-        f"Course_Enroled:{course}\n"
-        f"Academic_Year:{year}\n"
-    )
+def registration_datails(Student_ID, Student_Name, Course_Enroled, Academic_Year):
+    result = {
+        "Student_ID": f"{Student_ID}\n",
+        "Student_Name": f"{Student_Name}\n",
+        "Course_Enroled": f"{Course_Enroled}\n",
+        "Academic_Year": Academic_Year
+    }
     return result
+
+
 if __name__ == "__main__":
-    student_id="101"
-    student_name="Alice"
-    course_enroled="abc"
-    academic_year=2000
-    print(registration_details(student_id,student_name,course_enroled,academic_year))
+    student_id = "101"
+    student_name = "Alice"
+    course_enroled = "abc"
+    academic_year = 2000
+
+    print(registration_datails(student_id, student_name, course_enroled, academic_year))
