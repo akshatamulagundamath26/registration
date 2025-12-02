125b791fcf52186879aaa933d3458997606ec366
akshatamulagundamath26 <akshatamulagundamath26@gmail.com>
2025-12-02 12:00:36 +0530

    Create test_registration.py

---
+++ test_registration.py
@@ -0,0 +1,9 @@
+from registration import registration_datails
+def test_registration_details():
+    expected_uotput=(
+        Student_ID:"101\n"
+        Student_Name:"Alice\n"
+        Course_Enroled:"abc\n"
+        Academic_Year:2000
+        )
+assert registration_datails("101","Alice","abc",2000)==expected_output
